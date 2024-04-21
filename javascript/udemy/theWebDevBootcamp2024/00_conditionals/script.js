c0ec2b76163dc06210ab83961ae03106d54ea363
lfboeff <116302334+lfboeff@users.noreply.github.com>
2024-04-21 12:27:48 -0300

    add first udemy/javascript course

---
+++ javascript/udemy/theWebDevBootcamp2024/00_conditionals/script.js
@@ -0,0 +1,61 @@
+// let message = "    THIS IS THE MESSAGE!     ";
+// console.log(message);
+// console.log(message.trim().toLowerCase());
+
+// const password = prompt("Please enter a new password");
+// if (password.length >= 6) {
+//     if (password.indexOf(" ") === -1) {
+//         console.log("Valid password.")
+//     } else {
+//         console.log("Password cannot contain spaces.")
+//     }
+// } else {
+//     console.log("Password is too short! It must be 6+ characters.")
+// }
+
+// const age = prompt("What is your age?");
+// if (age >= 0) {
+//     if (age < 5 || age >= 65) {
+//         console.log("FREE");
+//     } else if (age < 10) {
+//         console.log("$10");
+//     } else if (age < 65) {
+//         console.log("$20");
+//     }
+// } else {
+//     console.log("INVALID AGE");
+// }
+
+// let firstName = prompt("Enter your first name:");
+// if (!firstName) {
+//     firstName = prompt("TRY AGAIN! Enter your first name:");
+// }
+// console.log(firstName)
+
+// let day = prompt("Enter an integer number:");
+// switch (parseInt(day)) {
+//     case 1:
+//         console.log("MONDAY");
+//         break;
+//     case 2:
+//         console.log("TUESDAY")
+//         break;
+//     case 3:
+//         console.log("WEDNESDAY")
+//         break;
+//     case 4:
+//         console.log("THURSDAY")
+//         break;
+//     case 5:
+//         console.log("FRIDAY")
+//         break;
+//     case 6:
+//         console.log("SATURDAY")
+//         break;
+//     case 7:
+//         console.log("SUNDAY")
+//         break;
+//     default:
+//         console.log("INVALID NUMBER")
+// }
+
